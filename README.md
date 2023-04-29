f207e6c42cb50b0c7853908958fef4c6d73705c0
Darlan Alves <me@darlanalv.es>
2023-04-29 23:13:48 +0200

    feat: add event source stream for progress

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ Unleash the power of AI and Automation!
 | API_KEY   | Chat API key           | true     |
 | API_MODEL | Chat model to use      | true     |
 | PORT      | http port              | true     |
+
+## Stream events
+
+|name | description |
+|-|-|
+| `cancel` | Current task was cancelled |
+| `next` | AI responded with a new step  |
+| `results` | Output of commands that were executed |
+| `error` | Error from last executed commands |
+| `halt` | AI has no more commands to execute |
+| `history` | Session was completed |
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
 import { IncomingMessage, createServer, ServerResponse } from 'http';
 import { createSession, updatePrimer, findCodeBlocks, getResponse, runCommands } from './utils.js';
 import logger from './file-logger.js';
+import { randomUUID } from 'crypto';
+
+let streams = new Map();
+
+const sendEvent = (uid, eventName, data) => {
+  if (!streams.has(uid)) { return; }
+
+  const stream = streams.get(uid);
+  stream.write('event: ' + eventName);
+  stream.write('data: ' + JSON.stringify(data));
+};
 
 export async function onRequest(request: IncomingMessage, response: ServerResponse) {
   if (request.url === '/favicon.ico') {
     response.writeHead(404);
     response.end();
     return;
+  }
+
+  if (request.url.startsWith('/events?') && request.method === 'GET') {
+    const uid = new URL('http://localhost/events?uid=123').searchParams.get('uid');
+    if (uid) {
+      streams.set(uid, response);
+      response.writeHead(202);
+      response.setHeader('Content-Type', 'text/event-stream');
+      response.setHeader('Cache-Control', 'no-cache');
+      return;
+    }
   }
 
   if (request.method !== 'POST') {
@@ -22,19 +44,34 @@ export async function onRequest(request: IncomingMessage, response: ServerRespon
     return;
   }
 
+  if (request.url === '/issues') {
+    const body = await readBody(request);
+    response.writeHead(202);
+    response.end();
+    console.log(JSON.parse(body));
+    return;
+  }
+
   if (request.url !== '/task') {
     response.writeHead(404);
     response.end();
     return;
   }
+  
+  const uid = randomUUID();
+  (request as any).uid = uid;
+  response.setHeader('Session-ID', uid);
+  response.writeHead(201);
 
   try {
-    runTask(request, response);
+    const sessionJson = runTask(uid, request, response);
+    response.end(sessionJson);
   } catch (error) {
-    response.writeHead(500);
+    sendEvent('error', String(error));
     response.end(String(error));
     logger.log('ERROR: ' + String(error));
-    console.log(error);
+  } finally {
+    streams.delete(uid);
   }
 }
 
@@ -46,20 +83,20 @@ async function readBody(request: IncomingMessage): Promise<string> {
   });
 }
 
-async function runTask(request: IncomingMessage, response: ServerResponse) {
-  const body = await readBody(request);
+async function runTask(uid: string, request: IncomingMessage, response: ServerResponse) {
   let waiting = true;
+  let maxCycles = 5;
 
   request.on('close', () => (waiting = false));
   request.on('error', () => (waiting = false));
 
+  const body = await readBody(request);
   const session = createSession(body);
-  logger.log('START: ' + body);
-  let maxCycles = 10;
 
   while (maxCycles) {
     if (!waiting) {
       logger.log('CANCELLED');
+      sendEvent(uid, 'cancel', null);
       break;
     }
 
@@ -70,41 +107,45 @@ async function runTask(request: IncomingMessage, response: ServerResponse) {
     });
 
     const commands = findCodeBlocks(completion);
+    sendEvent(uid, 'next', completion);
     logger.log('NEXT: ' + completion);
     logger.log('COMMANDS:\n' + commands.join('\n'));
 
     if (!commands.length) {
       logger.log('HALT');
+      sendEvent(uid, 'halt', null);
       break;
     }
 
     const run = await runCommands(commands);
 
     if (run.ok) {
+      const runOutput = run.outputs.map((o) => ['# ' + o.cmd, (o.output.stdout || 'no output')].join('\n')).join('\n\n');
+      sendEvent(uid, 'results', runOutput);
       session.messages.push({
         role: 'user',
-        content:
-          'These are the results:\n' +
-          run.outputs.map((o) => ['# ' + o.cmd, (o.output.stdout || 'no output')].join('\n')).join('\n\n') +
-          '\n\nAnything else?',
+        content: `Results:\n${runOutput}\n\nAnything else?`,
       });
     }
 
     if (!run.ok) {
       const lastCmd = run.outputs[run.outputs.length - 1];
       const error = String(lastCmd.output.error || lastCmd.output.stderr);
-
+      sendEvent(uid, 'error', error);
       maxCycles--;
+    
       session.messages.push({
         role: 'user',
-        content: `The command:\n\`${lastCmd.cmd}\`\n has failed with this error:\n${error}\nFix the command and write in the next instruction.`,
+        content: `The command \`${lastCmd.cmd}\`  has failed with this error:\n${error}\nFix the command and write in the next instruction.`,
       });
     }
   }
 
   const sessionJson = JSON.stringify(session.messages.slice(3));
   logger.log('END: ' + sessionJson);
-  response.end(sessionJson);
+  sendEvent(uid, 'history', session.messages.slice(3));
+  
+  return sessionJson;
 }
 
 if (process.env.PORT) {
